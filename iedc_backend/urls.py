8c5aeb98767efd74d785b380f797491726a362e0
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-11-24 22:53:44 +0530

    urlchanged

--- iedc_backend/urls.py
+++ iedc_backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/iedc/', include('dashboard.urls')),
+    path('api/v1/cusat/', include('dashboard.urls')),
 ]
